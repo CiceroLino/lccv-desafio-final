a6465b80214f5ec943127c57aab33ee767cab906
Cícero Lino <cicerolinoeneto@gmail.com>
2021-12-12 23:14:08 -0300

    Add docker to project and fix front-end

--- front-end/src/app/modules/contratos/contratos-routing.module.ts
+++ front-end/src/app/modules/contratos/contratos-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { IndexComponent } from './components/index/index.component';
 
-//.. /contratos..
 const routes: Routes = [
   {
     path: '', component: IndexComponent
